22a3eee8a6d0612951abcc6ad7af65ae87724dc7
BruceLuo <295448382@qq.com>
2024-05-06 23:46:12 +0800

    feat: connections page continue

--- src/app/(main)/(pages)/connections/page.tsx
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -1,10 +1,50 @@
 import React from "react";
+import { CONNECTIONS } from '@/lib/constant'
 
 type Props = {
   searchParams?: { [key: string]: string | undefined };
 };
 
 const Connections = async (props: Props) => {
+  const {
+    webhook_id,
+    webhook_name,
+    webhook_url,
+    guild_id,
+    guild_name,
+    channel_id,
+    access_token,
+    workspace_name,
+    workspace_icon,
+    workspace_id,
+    database_id,
+    app_id,
+    authed_user_id,
+    authed_user_token,
+    slack_access_token,
+    bot_user_id,
+    team_id,
+    team_name,
+  } = props.searchParams ?? {
+    webhook_id: '',
+    webhook_name: '',
+    webhook_url: '',
+    guild_id: '',
+    guild_name: '',
+    channel_id: '',
+    access_token: '',
+    workspace_name: '',
+    workspace_icon: '',
+    workspace_id: '',
+    database_id: '',
+    app_id: '',
+    authed_user_id: '',
+    authed_user_token: '',
+    slack_access_token: '',
+    bot_user_id: '',
+    team_id: '',
+    team_name: '',
+  }
   return (
     <div className="relative flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -14,7 +54,18 @@ const Connections = async (props: Props) => {
         <section className="flex flex-col gap-4 p-6 text-muted-foreground">
           Connect all your apps directly from here. You may need to connect
           these apps regularly to refresh verification
+          {/* {CONNECTIONS.map((connection) => (
+            <ConnectionCard
+              key={connection.title}
+              description={connection.description}
+              title={connection.title}
+              icon={connection.image}
+              type={connection.title}
+              connected={connections}
+            />
+          ))} */}
         </section>
+        
       </div>
     </div>
   );
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
-// import { ConnectionProviderProps } from '@/providers/connections-provider'
+import { ConnectionProviderProps } from '@/providers/connections-provider'
 import { z } from 'zod'
 
 export const EditUserProfileSchema = z.object({
@@ -13,15 +13,15 @@ export const WorkflowFormSchema = z.object({
 
 export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord'
 
-// export type Connection = {
-//   title: ConnectionTypes
-//   description: string
-//   image: string
-//   connectionKey: keyof ConnectionProviderProps
-//   accessTokenKey?: string
-//   alwaysTrue?: boolean
-//   slackSpecial?: boolean
-// }
+export type Connection = {
+  title: ConnectionTypes
+  description: string
+  image: string
+  connectionKey: keyof ConnectionProviderProps
+  accessTokenKey?: string
+  alwaysTrue?: boolean
+  slackSpecial?: boolean
+}
 
 export type EditorCanvasTypes =
   | 'Email'
